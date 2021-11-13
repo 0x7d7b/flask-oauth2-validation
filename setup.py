6405a2c635267e3299c5702e3e7b1746e7b0a9f8
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2021-11-13 11:03:49 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='flask-oauth2-api',
+    packages=['flask-oauth2-api'],
+    version='0.1.0',
+    author='Henrik Sachse',
+    author_email='henrik@tuta.io',
+    url='https://github.com/0x7d7b',
+    license='BSD',
+    description='OAuth2 access token verification for resource servers',
+    include_package_data=True,
+    install_requires=[
+
+    ]
+)
