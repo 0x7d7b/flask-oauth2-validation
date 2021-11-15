eae390aff68830f4d7110a61368eff9e16952b7c
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2021-11-15 22:13:18 +0100

    Test improvements

--- tests/test_init.py
+++ tests/test_init.py
@@ -22,6 +22,7 @@ def test_issuer_only(test_app):
     app = test_app(meta_data=True, jwks_uri=True)
     app.config['OAUTH2_ISSUER'] = 'https://issuer.local/oauth2'
     oauth2 = OAuth2Decorator(app)
+    assert oauth2._issuer == 'https://issuer.local/oauth2'
     assert oauth2._jwks_uri == 'https://issuer.local/oauth2/keys'
     assert oauth2._issuer_public_keys == mocked_keys
 
@@ -36,8 +37,28 @@ def test_issuer_and_jwks_uri_configured(test_app):
     app.config['OAUTH2_ISSUER'] = 'https://issuer.local/oauth2'
     app.config['OAUTH2_JWKS_URI'] = 'https://issuer.local/oauth2/keys'
     oauth2 = OAuth2Decorator(app)
+    assert oauth2._issuer == 'https://issuer.local/oauth2'
     assert oauth2._jwks_uri == 'https://issuer.local/oauth2/keys'
     assert oauth2._issuer_public_keys == mocked_keys
+
+
+def test_issuer_and_jwks_refresh_configured(test_app):
+    """ The OAUTH2_ISSUER and OAUTH2_JWKS_URIconfig attribute have been set.
+    Thus we expect no authorization server metadata lookup to retrieve
+    the jwks_uri. Only the jwks_uri should be requested to retrieve all
+    public keys the authorization server uses.
+    """
+    app = test_app(meta_data=True, jwks_uri=True)
+    app.config['OAUTH2_ISSUER'] = 'https://issuer.local/oauth2'
+    app.config['OAUTH2_JWKS_URI'] = 'https://issuer.local/oauth2/keys'
+    app.config['OAUTH2_JWKS_UPDATE_INTERVAL'] = 1234
+    oauth2 = OAuth2Decorator(app)
+    assert oauth2._issuer == 'https://issuer.local/oauth2'
+    assert oauth2._jwks_uri == 'https://issuer.local/oauth2/keys'
+    assert oauth2._issuer_public_keys == mocked_keys
+    assert oauth2._jwks_update_interval == 1234
+    assert oauth2._executor
+    assert oauth2._jwks_last_update_timestamp
 
 
 def test_introspection_setup(test_app):
@@ -54,6 +75,9 @@ def test_introspection_setup(test_app):
     app.config['OAUTH2_CLIENT_ID'] = 'foo-client'
     app.config['OAUTH2_CLIENT_SECRET'] = 'very-secure'
     oauth2 = OAuth2Decorator(app)
+    assert oauth2._issuer == 'https://issuer.local/oauth2'
+    assert oauth2._client_id == 'foo-client'
+    assert oauth2._client_secret == 'very-secure'
     assert oauth2._introspection_endpoint == 'https://issuer.local/oauth2/introspect'
     assert oauth2._introspection_auth_method == 'client_secret_post'
 
@@ -82,5 +106,8 @@ def test_introspection_setup_with_endpoint(test_app):
     app.config['OAUTH2_CLIENT_SECRET'] = 'very-secure'
     app.config['OAUTH2_OAUTH2_INTROSPECTION_ENDPOINT'] = 'https://issuer.local/oauth2/introspect'
     oauth2 = OAuth2Decorator(app)
+    assert oauth2._issuer == 'https://issuer.local/oauth2'
+    assert oauth2._client_id == 'foo-client'
+    assert oauth2._client_secret == 'very-secure'
     assert oauth2._introspection_endpoint == 'https://issuer.local/oauth2/introspect'
     assert oauth2._introspection_auth_method == 'client_secret_post'
