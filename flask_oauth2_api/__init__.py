a0f377f064386814229a91639b17cae7a6e36acd
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2021-11-20 11:01:22 +0100

    Added the MIT license

--- flask_oauth2_api/__init__.py
+++ flask_oauth2_api/__init__.py
@@ -2,6 +2,6 @@ from .decorator import OAuth2Decorator
 
 __version__ = "0.1.0"
 __author__ = "Henrik Sachse"
-__mail__ = "0x7d7b@users.noreply.github.com"
+__mail__ = "henrik@0x7d7b.net"
 __homepage__ = 'https://github.com/0x7d7b'
-__license__ = 'BSD'
+__license__ = 'MIT'
