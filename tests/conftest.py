d6ded42857388748274d67619f6fb03344661422
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2021-11-20 22:34:43 +0100

    Cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,8 @@ from flask import Flask
 
 import pytest
 import requests_mock
-import logging
 
 from functional import test_jwk
-
-_test_logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope='function')
@@ -14,42 +11,12 @@ def test_app():
     with requests_mock.Mocker() as mock:
         _register_mock_addresses(mock)
         yield _create_flask_app()
-        try:
-            _assert_request_history()
-        except BaseException:
-            # Just in case catch exceptions at teardown.
-            pass
 
 
 def _create_flask_app():
     app = Flask(__name__)
     app.config['TESTING'] = True
     return app
-
-
-def _assert_request_history(
-    mock: requests_mock.Mocker
-):
-    assert _was_requested(
-        mock,
-        'GET https://issuer.local/oauth2' +
-        '/.well-known/oauth-authorization-server'
-    )
-    assert _was_requested(
-        mock,
-        'GET https://issuer.local/oauth2/keys'
-    )
-
-
-def _was_requested(
-    mock: requests_mock.Mocker,
-    uri: str
-):
-    for request_proxy in mock.request_history:
-        if str(request_proxy) == uri:
-            _test_logger.debug(f'Verified request: {uri}')
-            return True
-    return False
 
 
 def _register_mock_addresses(
