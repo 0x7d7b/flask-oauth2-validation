527607d70869b91d3b261e71bb295ad367017cd7
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2021-11-22 22:59:02 +0100

    Syntax error fixed in setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'Bug Tracker':
             'https://github.com/0x7d7b/flask-oauth2-validation/issues',
         'Source':
-            'https://github.com/0x7d7b/flask-oauth-validation/'
+            'https://github.com/0x7d7b/flask-oauth-validation/',
         'Test Coverage':
             'https://codecov.io/gh/0x7d7b/flask-oauth2-validation'
     },
