7b65c18bab40616def2b45f28db2ac6b5cd59c18
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2021-11-23 21:34:11 +0100

    Extracted exceptions to separate file

--- src/flask_oauth2_validation/decorator.py
+++ src/flask_oauth2_validation/decorator.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request
 from flask_executor import Executor
 from functools import wraps
 from jwt import JWT, jwk_from_dict
@@ -10,53 +10,10 @@ import requests
 import time
 import threading
 
-
-class OAuth2Exception(BaseException):
-    """ Exception for building up an HTTP error response.
-    In case an error occurs the OAuth2Exception attributes
-    hold information which are being taken over into the
-    json error response.
-    """
-
-    def __init__(self, status_code: int, error: str, error_message: str):
-        self.status_code = status_code
-        self.error = error
-        self.error_message = error_message
-
-    def response(self):
-        response = make_response()
-        response.headers.pop('Content-Type', None)
-        response.status_code = self.status_code
-        response.headers['WWW-Authenticate'] = ' '.join([
-            'Bearer',
-            'error="' + self.error + '"',
-            self._error_description()
-        ])
-        return response
-
-    def _error_description(self):
-        return 'error_description="' + self.error_message + '"'
-
-
-class OAuth2BadRequestException(OAuth2Exception):
-
-    def __init__(self, error_message: str):
-        super().__init__(400, 'invalid_request', error_message)
-
-
-class OAuth2InvalidTokenException(OAuth2Exception):
-
-    def __init__(self, error_message: str):
-        super().__init__(401, 'invalid_token', error_message)
-
-
-class OAuth2InsufficientScopeException(OAuth2Exception):
-
-    def __init__(self, missing_scope: str):
-        super().__init__(403, 'insufficient_scope', missing_scope)
-
-    def _error_description(self):
-        return 'scope="' + self.error_message + '"'
+from .exceptions import OAuth2BadRequestException, \
+    OAuth2InsufficientScopeException, \
+    OAuth2InvalidTokenException, \
+    OAuth2Exception
 
 
 class OAuth2Decorator():
