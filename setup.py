d1310e11c64fbea4369f9d021d59fb7729f88796
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2023-01-22 15:21:17 +0100

    0.1.2 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='flask-oauth2-validation',
-    version='0.1.2',
+    version='0.1.3',
     author='Henrik Sachse',
     author_email='henrik@0x7d7b.net',
     description=(
